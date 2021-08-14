f538db7280e53f8a7dcf13b1c4d97f167e708d1c
jer-myah <aluefuaehiabhi@gmail.com>
2021-08-14 19:45:51 +0100

    commented out duration

--- resources/js/Pages/Admin/Links/Index.vue
+++ resources/js/Pages/Admin/Links/Index.vue
@@ -215,7 +215,7 @@
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <!-- <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"> -->
                                             <span class="text-sm text-gray-500">
-                                            {{ link.duration }}
+                                            <!-- {{ link.duration }} -->
                                             </span>
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap">
